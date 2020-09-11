13a491c41a845e69b6306f44487aea1e03ebdc4d
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-09-11 12:54:16 +0200

    final refactor

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->post('/confirm', ConfirmController::class);
+Route::middleware('auth:sanctum')->post('/confirm', 'ConfirmController');
